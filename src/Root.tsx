2d146575d1d23b69c700b1891358c0128c7d190a
MMG <exprescime@gmail.com>
2026-09-22 20:08:43 +0300

    Register AIClip composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,18 +1,35 @@
 import {ALL_FORMATS, Input, UrlSource} from "mediabunny";
 import {Composition, staticFile} from "remotion";
 import {Visualizer} from "./Visualizer/Main";
+import {AIClip} from "./AIClip/Main";
 import {SON_YAPRAK} from "./data/son-yaprak";
+import {AI_PROJECT} from "./data/ai-project";
 
-export const RemotionRoot: React.FC = () => <Composition
-  id="Visualizer"
-  component={Visualizer}
-  width={1920}
-  height={1080}
-  durationInFrames={Math.ceil(SON_YAPRAK.durationSeconds * SON_YAPRAK.fps)}
-  fps={SON_YAPRAK.fps}
-  calculateMetadata={async () => {
-    const input = new Input({source: new UrlSource(staticFile(SON_YAPRAK.audio)), formats: ALL_FORMATS});
-    const durationInSeconds = await input.computeDuration();
-    return {durationInFrames: Math.ceil(durationInSeconds * SON_YAPRAK.fps), fps: SON_YAPRAK.fps};
-  }}
-/>;
+const metadataFor = (audio: string, fps: number) => async () => {
+  const input = new Input({source: new UrlSource(staticFile(audio)), formats: ALL_FORMATS});
+  const durationInSeconds = await input.computeDuration();
+  return {durationInFrames: Math.ceil(durationInSeconds * fps), fps};
+};
+
+export const RemotionRoot: React.FC = () => (
+  <>
+    <Composition
+      id="Visualizer"
+      component={Visualizer}
+      width={1920}
+      height={1080}
+      durationInFrames={Math.ceil(SON_YAPRAK.durationSeconds * SON_YAPRAK.fps)}
+      fps={SON_YAPRAK.fps}
+      calculateMetadata={metadataFor(SON_YAPRAK.audio, SON_YAPRAK.fps)}
+    />
+    <Composition
+      id="AIClip"
+      component={AIClip}
+      width={1920}
+      height={1080}
+      durationInFrames={Math.ceil(AI_PROJECT.durationSeconds * AI_PROJECT.fps)}
+      fps={AI_PROJECT.fps}
+      calculateMetadata={metadataFor(AI_PROJECT.audio, AI_PROJECT.fps)}
+    />
+  </>
+);
